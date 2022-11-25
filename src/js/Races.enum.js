f13ca05ecd2fe0baf2b3bbf186cffeb36f818e8e
Slash <slash@slashware.net>
2022-11-24 20:33:06 -0500

    Decoupling tests

--- src/js/Races.enum.js
+++ src/js/Races.enum.js
@@ -1,8 +1,10 @@
 module.exports = {
 	RAT: {
-		tile: new ut.Tile('r', 128, 128, 0)
+		tile: new ut.Tile('r', 128, 128, 0),
+		name: 'Rat'
 	},
 	TROLL: {
-		tile: new ut.Tile('T', 128, 128, 0)
+		tile: new ut.Tile('T', 128, 128, 0),
+		name: 'Troll'
 	},
 }
